30fd7143d6906f7b9a5d3ea70de7c72ebc6f9ec2
Douglas Campbell <dacampbelltbone@gmail.com>
2019-05-28 21:48:45 -0400

    added search feature, day 2 MVP complete.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      posts: []
+      posts: [],
+      postsFiltered: []
     }
   };
 
@@ -18,13 +19,31 @@ class App extends Component {
       likes: dummyData.likes
     })
   };
+
+  handlePostSearch = e => {
+    const posts = this.state.posts.filter(post => {
+      if (post.username.includes(e.target.value)) {
+        return post;
+      }
+    });
+    this.setState({
+      postsFiltered: posts
+    });
+  };
   
 
   render() {
     return (
       <div className="App">
-        <SearchBar />
-        <PostContainer posts={this.state.posts} comments={this.state.comments} likes={this.state.likes}/>
+        <SearchBar 
+          postSearch={this.handlePostSearch}
+          lookupSearch={this.state.termSearch}
+        />
+        <PostContainer 
+          posts={this.state.postsFiltered.length > 0 ? this.state.postsFiltered : this.state.posts} 
+          comments={this.state.comments} 
+          likes={this.state.likes}
+        />
       </div>
     );
   
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -3,11 +3,11 @@ import './SearchBar.css';
 import IGLogo from '../../logo/logo.instagram.png';
 
 
-const SearchBar = () => {
+const SearchBar = props => {
     return (
         <div className="search">
             <img id="insta" src={IGLogo} alt="logo"/>
-            <input type="text" name="search" placeholder="Search"/>
+            <input type="text" name="search" onKeyDown={props.postSearch} placeholder="Search"/>
             <div className="social">
                 <span><img src="https://img.icons8.com/material-outlined/36/000000/compass.png" alt="compass"/></span>
                 <span><img src="https://img.icons8.com/windows/36/000000/hearts.png" alt="like"/></span>

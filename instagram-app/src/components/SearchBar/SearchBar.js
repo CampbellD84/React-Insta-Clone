0c9d6be5e19ce111f917e484d86592b12f6f5d2a
Douglas Campbell <dacampbelltbone@gmail.com>
2019-05-30 22:14:31 -0400

    Added styled-components, refactored SearchBar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,53 @@
 import React from 'react';
-import './SearchBar.css';
 import IGLogo from '../../logo/logo.instagram.png';
+import styled from 'styled-components';
 
+const SearchHeader = styled.header`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin: 0;
+    background-color: #fff;
+    width: 100%;
+    border-bottom: 1px solid #2d2d37;
+`;
+
+const InstaLogo = styled.img`
+    width: 220px;
+    height: 60px;
+`;
+
+const SocialIcons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const SearchInput = styled.input`
+    border: 0;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-color: #f0f0f0;
+    height: 30px;
+    width: 300px;
+    border-radius: 5px;
+    ::placeholder {
+        padding-left: 10px;
+    }
+}
+`;
 
 const SearchBar = props => {
     return (
-        <div className="search">
-            <img id="insta" src={IGLogo} alt="logo"/>
-            <input type="text" name="search" onKeyDown={props.postSearch} placeholder="Search"/>
-            <div className="social">
+        <SearchHeader>
+            <InstaLogo src={IGLogo} alt="instagram logo"/>
+            <SearchInput type="text" name="search" onKeyDown={props.postSearch} placeholder="Search"/>
+            <SocialIcons>
                 <span><img src="https://img.icons8.com/material-outlined/36/000000/compass.png" alt="compass"/></span>
                 <span><img src="https://img.icons8.com/windows/36/000000/hearts.png" alt="like"/></span>
                 <span><img src="https://img.icons8.com/windows/36/000000/contacts.png" alt="user"/></span>
-            </div>
-        </div>
+            </SocialIcons>
+        </SearchHeader>
     );
 }
 
